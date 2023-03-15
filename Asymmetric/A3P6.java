aa7bbc6aca494cf2b70b4bd4d1de0d6976ed88af
Ronny Blostein <ronnybl1999@gmail.com>
2023-03-15 16:44:20 -0400

    Update A3P6.java

--- Asymmetric/A3P6.java
+++ Asymmetric/A3P6.java
@@ -6,6 +6,15 @@ import java.security.MessageDigest;
 import util.CryptoTools;
 
 public class A3P6 {
+	/*Alice and Bob are using the Diffie Hellman key agreement protocol to establish a shared key. 
+	They agree publicly on the algorithm parameters p and g. Alice then picks a private key aX and Bob picks bX. Here is the data:
+
+	BigInteger p = new BigInteger("341769842231234673709819975074677605139");
+	BigInteger g = new BigInteger("37186859139075205179672162892481226795");
+	BigInteger aX = new BigInteger("83986164647417479907629397738411168307");
+	BigInteger bX = new BigInteger("140479748264028247931575653178988397140");
+
+	They then compute and exchange their public keys in order to arrive at a shared session key. Determine that session key and write it as a hex string.*/
 	public static void main(String[] args) throws Exception{
 		BigInteger p = new BigInteger("341769842231234673709819975074677605139");
 		BigInteger g = new BigInteger("37186859139075205179672162892481226795");
