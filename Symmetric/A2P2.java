a1c0bc0fd559a429bcdcfd823454b883a5a086b6
Ronny Blostein <ronnybl1999@gmail.com>
2023-03-15 16:25:37 -0400

    Update A2P2.java

--- Symmetric/A2P2.java
+++ Symmetric/A2P2.java
@@ -9,6 +9,15 @@ import javax.crypto.spec.SecretKeySpec;
 import util.CryptoTools;
 
 public class A2P2 {
+	/*An English string is encrypted with 128-bit AES using PKCS5 padding, the CBC mode of operation, and the following parameters:
+
+      	KEY = "DO NOT TELL EVE!"  (expressed as an English string)
+      	IV  = 0x 20FC19123087BF6CAC8D0F1254123004 
+
+	Given that the ciphertext is:
+      	0x 3188073EA5DB3F5C05B6307B3595607135F5D4B22F2C3EB710AA31377F78B997 
+
+	determine the characters of the plaintext and output them as English string, not in hex or binary. What is the first word of the plaintext?*/
 	public static void main(String[] args) throws Exception{
 		String keyString = "DO NOT TELL EVE!";
 		String ivString = "20FC19123087BF6CAC8D0F1254123004";
