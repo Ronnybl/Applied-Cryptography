50f617ac1f9ae7cd2485373f37db2c3dc0bb6c6b
Ronny Blostein <ronnybl1999@gmail.com>
2023-03-15 16:56:08 -0400

    Update T4Q4.java

--- Hash/T4Q4.java
+++ Hash/T4Q4.java
@@ -10,6 +10,23 @@ import javax.crypto.spec.SecretKeySpec;
 import util.CryptoTools;
 
 public class T4Q4 {
+	/*
+	We have discussed in class how to use the AES (or any block cipher) to realize/compute MAC, as depicted in the following figure:
+
+	[AESMAC]
+
+	where the final/last output of the cipher is considered to be the computed MAC value.
+
+	Compute the MAC value of the following message:
+
+	m = Why do we tell actors to break a leg? because every play has a cast
+	Using the following information:
+
+	e (from the figure) = AES in Electronic Code Book mode with no padding
+	k (from the figure, in hex) =  34567abcdef0321134567abcdef03211
+	iv (from the figure, in hex) = 44668abddef1321134568abcdef13221
+
+	What's the MAC value? provide the Java code you have developed to answer this question.*/
 	public static void main(String[] args) throws Exception{
 		byte[] m = "Why do we tell actors to break a leg? because every play has a cast".getBytes();
 		byte[] k = CryptoTools.hexToBytes("34567abcdef0321134567abcdef03211");
